3186c816c2e008c33fc5cf8ce60beb667b9f26ba
Gilles Caulier <caulier.gilles@gmail.com>
2007-01-26 20:40:38 +0000

    kipi-plugins from trunk : all plugins use the new shared library libkexiv2 instead KipiPlugins::EXiv2Iface class.
    Marcel, next stage is to derivate the DigiKam::DMetadata class from LibKExiv2. Let's me hear when you whant to do it...
    
    CCMAIL: digikam-devel@kde.org, kde-imaging@kde.org, marcel.wiesweg@gmx.de
    
    
    svn path=/trunk/extragear/libs/libkexiv2/; revision=627501

--- libkexiv2.h
+++ libkexiv2.h
@@ -6,10 +6,6 @@
  *
  * Copyright 2006-2007 by Gilles Caulier and Marcel Wiesweg
  *
- * NOTE: This class is a simplified version of Digikam::DMetadata
- *       class from digiKam core. Please contact digiKam team 
- *       before to change/fix/improve this implementation.
- *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
