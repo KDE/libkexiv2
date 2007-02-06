37d766a0343034bc3f41459eeae2f3e87ba43842
Gilles Caulier <caulier.gilles@gmail.com>
2007-02-06 22:04:18 +0000

    libkexiv2 from trunk : add version header file for this library CCMAIL: Achim Bohnet <ach@mpe.mpg.de>
    svn path=/trunk/extragear/libs/libkexiv2/; revision=630967

---
+++ version.h
@@ -0,0 +1,29 @@
+/* ============================================================
+ * Authors: Gilles Caulier <caulier dot gilles at free.fr>
+ * Date   : 2007-02-06
+ * Description : libkexiv2 release ID header.
+ * 
+ * Copyright 2007 by Gilles Caulier
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
+
+#ifndef KEXIV2_VERSION_H
+#define KEXIV2_VERSION_H
+
+static const char kipi_version[] = "0.1.0";
+
+#define KEXIV2_VERSION 0x000100
+
+#endif // KEXIV2_VERSION_H
+
