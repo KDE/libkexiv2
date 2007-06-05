e12f226ff521200f9b480e08dcfcc28bb01a417d
Laurent Montel <montel@kde.org>
2007-06-05 07:01:56 +0000

    Generate and install libkexiv2
    svn path=/trunk/extragear/libs/libkexiv2/; revision=671627

---
+++ libkexiv2.pc.cmake
@@ -0,0 +1,11 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${BIN_INSTALL_DIR}
+libdir=${LIB_INSTALL_DIR}
+includedir=${INCLUDE_INSTALL_DIR}
+
+Name: libkexiv2
+Description: KDE wrapper library for Exiv2 library with some extras
+Requires:
+Version: 0.3.0
+Libs: -L${LIB_INSTALL_DIR} -lkexiv2
+Cflags: -I${INCLUDE_INSTALL_DIR}
