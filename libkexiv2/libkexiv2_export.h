c130013b877d128a72e86048ba0ec2dcc91e8a92
Laurent Montel <montel@kde.org>
2007-06-18 21:02:52 +0000

    Regenerate it
    svn path=/trunk/extragear/libs/libkexiv2/; revision=677296

--- libkexiv2/libkexiv2_export.h
+++ libkexiv2/libkexiv2_export.h
@@ -1,45 +1,40 @@
-/* ============================================================
- *
- * This file is a part of kipi-plugins project
- * http://www.kipi-plugins.org
- *
- * Date        : 2005-09-15
- * Description : Exiv2 library interface for KDE
- *
- * Copyright (C) 2005,2006,2007 by Laurent Montel <montel@kde.org>
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * ============================================================ */
+/*  This file is part of the KDE project
+    Copyright (C) 2007 David Faure <faure@kde.org>
 
-#ifndef _LIBKEXIV2_EXPORT_H
-#define _LIBKEXIV2_EXPORT_H
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef LIBKEXIV2_EXPORT_H
+#define LIBKEXIV2_EXPORT_H
+
+/* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 
-#ifdef Q_WS_WIN
-
 #ifndef KEXIV2_EXPORT
-# ifdef MAKE_KEXIV2_LIB
+# if defined(MAKE_KEXIV2_LIB)
+   /* We are building this library */ 
 #  define KEXIV2_EXPORT KDE_EXPORT
 # else
+   /* We are using this library */ 
 #  define KEXIV2_EXPORT KDE_IMPORT
 # endif
 #endif
 
-#else // not windows
-
-#define KEXIV2_EXPORT KDE_EXPORT
+# ifndef KEXIV2_EXPORT_DEPRECATED
+#  define KEXIV2_EXPORT_DEPRECATED KDE_DEPRECATED KEXIV2_EXPORT
+# endif
 
 #endif
-
-#endif /* _LIBKEXIV2_EXPORT_H */
-
