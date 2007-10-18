8ebd0e0cce7c86a1a08ba28ddcf36cf078141fe0
Gilles Caulier <caulier.gilles@gmail.com>
2007-10-18 07:43:33 +0000

    update
    svn path=/trunk/extragear/libs/libkexiv2/; revision=726598

--- libkexiv2.lsm.cmake
+++ libkexiv2.lsm.cmake
@@ -2,8 +2,8 @@ Begin4
 Title: libkexiv2
 Version: ${KEXIV2_LIB_VERSION_STRING}
 Entered-date: 2007-05-08
-Description: An library to manipulate EXIF/IPTC information using Exiv2 library
-Keywords: kde image jpeg raw tiff png exif iptc digikam kipi-plugins
+Description: An library to manipulate EXIF/IPTC/XMP information using Exiv2 library
+Keywords: kde image jpeg raw tiff png exif iptc xmp digikam kipi-plugins
 Author: caulier dot gilles at gmail dot com (Gilles Caulier)
  marcel dot wiesweg at gmx dot de (Marcel Wiesweg)
 Maintained-by:
