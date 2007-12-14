1261b1dbf6ed38f8663983bbb0db0ca51cfda515
Gilles Caulier <caulier.gilles@gmail.com>
2007-12-14 14:11:22 +0000

    new version
    svn path=/branches/extragear/kde3/libs/libkexiv2/; revision=748437

--- libkexiv2/version.h
+++ libkexiv2/version.h
@@ -23,9 +23,9 @@
 #ifndef KEXIV2_VERSION_H
 #define KEXIV2_VERSION_H
 
-static const char kexiv2_version[] = "0.1.6";
+static const char kexiv2_version[] = "0.1.7";
 
-#define KEXIV2_VERSION 0x000106
+#define KEXIV2_VERSION 0x000107
 
 #endif // KEXIV2_VERSION_H
 
