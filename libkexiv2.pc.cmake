c2dc5e7a078dfc7831aeb4b0a0aa1ddf108e9372
Gilles Caulier <caulier.gilles@gmail.com>
2008-03-18 18:46:55 +0000

    update
    svn path=/trunk/extragear/libs/libkexiv2/; revision=787191

--- libkexiv2.pc.cmake
+++ libkexiv2.pc.cmake
@@ -5,6 +5,7 @@ includedir=${INCLUDE_INSTALL_DIR}
 
 Name: libkexiv2
 Description: A C++ library to manipulate EXIF/IPTC/XMP metadata using Exiv2 library. This library is used by digiKam and kipi-plugins.
+URL: http://www.kipi-plugins.org
 Requires:
 Version: ${KEXIV2_LIB_VERSION_STRING}
 Libs: -L${LIB_INSTALL_DIR} -lkexiv2
