bd0bd8bcbdd3af9d69e75ad87af8fd6acd56e164
Gilles Caulier <caulier.gilles@gmail.com>
2009-01-21 18:01:37 +0000

    update
    svn path=/trunk/KDE/kdegraphics/libs/libkexiv2/; revision=914756

--- libkexiv2.lsm.cmake
+++ libkexiv2.lsm.cmake
@@ -1,15 +1,15 @@
 Begin4
-Title: libkexiv2
-Version: ${KEXIV2_LIB_VERSION_STRING}
-Entered-date: 2007-05-08
-Description: A C++ library to manipulate EXIF/IPTC/XMP metadata using Exiv2 library. This library is used by digiKam and kipi-plugins.
-Keywords: kde image jpeg raw tiff png exif iptc xmp digikam kipi-plugins
-Author: caulier dot gilles at gmail dot com (Gilles Caulier)
- marcel dot wiesweg at gmx dot de (Marcel Wiesweg)
+Title:          libkexiv2
+Version:        ${KEXIV2_LIB_VERSION_STRING}
+Entered-date:   2007-05-08
+Description:    A C++ library to manipulate EXIF/IPTC/XMP metadata using Exiv2 library. This library is used by digiKam and kipi-plugins.
+Keywords:       kde image jpeg raw tiff png exif iptc xmp digikam kipi-plugins
+Author:         caulier dot gilles at gmail dot com (Gilles Caulier)
+                marcel dot wiesweg at gmx dot de (Marcel Wiesweg)
 Maintained-by:
-Primary-site: http://www.kipi-plugins.org
+Primary-site:   http://www.kipi-plugins.org
 Alternate-site: http://www.digikam.org
 Original-site:
-Platforms:
+Platforms:      Linux and other Unices, MacOs-X, Win32. Needs KDE 4.x
 Copying-policy: GPL
 End
