972604e4cfe4c2a2e152ff3aebe71acbd44ba055
Gilles Caulier <caulier.gilles@gmail.com>
2009-07-22 12:18:55 +0000

    include Template widgets from digiKam core in libkexiv2. These one will be avaialble with KDE 4.4. For the moment code is duplicated to safe digiKam with KDE < 4.4. Code will be removed from digiKam core after KDE 4.4 libkexiv2 has now strings to translate. CCMAIL: kde-i18n-doc@kde.org
    svn path=/trunk/KDE/kdegraphics/libs/libkexiv2/; revision=1000983

---
+++ libkexiv2/altlangstredit.h
@@ -0,0 +1,98 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2009-06-15
+ * Description : multi-languages string editor
+ *
+ * Copyright (C) 2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef ALTLANGSTREDIT_H
+#define ALTLANGSTREDIT_H
+
+// Qt includes
+
+#include <QtGui/QWidget>
+#include <QtCore/QString>
+
+// Local includes
+
+#include "kexiv2.h"
+#include "libkexiv2_export.h"
+
+namespace KExiv2Iface
+{
+
+class AltLangStrEditPriv;
+
+class KEXIV2_EXPORT AltLangStrEdit : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    AltLangStrEdit(QWidget* parent);
+    ~AltLangStrEdit();
+
+    void setTitle(const QString& title);
+    void setClickMessage(const QString& msg);
+
+    void setValues(const KExiv2::AltLangMap& values);
+    KExiv2::AltLangMap& values();
+
+    QString currentLanguageCode() const;
+    QString languageCode(int index) const;
+
+    QString defaultAltLang() const;
+    bool    asDefaultAltLang() const;
+
+    /** Reset all entries
+     */
+    void reset();
+
+    /** Force current text to be registered in captions map
+     */
+    void apply();
+
+    void setDirty(bool dirty);
+    bool isDirty() const;
+
+Q_SIGNALS:
+
+    void signalModified();
+    void signalSelectionChanged(const QString&);
+    void signalAddValue(const QString&, const QString&);
+    void signalDeleteValue(const QString&);
+
+protected Q_SLOTS:
+
+    void slotTextChanged();
+    void slotSelectionChanged(int);
+    void slotAddValue();
+    void slotDeleteValue();
+
+protected:
+
+    void loadLangAltListEntries(const QString& currentLang=QString("x-default"));
+
+private:
+
+    AltLangStrEditPriv* const d;
+};
+
+}  // namespace KExiv2Iface
+
+#endif // ALTLANGSTREDIT_H
---
+++ libkexiv2/countryselector.h
@@ -0,0 +1,65 @@
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2009-07-07
+ * Description : country selector combo-box.
+ *
+ * Copyright (C) 2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef COUNTRY_SELECTOR_H
+#define COUNTRY_SELECTOR_H
+
+// Qt includes
+
+#include <QtCore/QString>
+#include <QtGui/QWidget>
+
+// KDE includes
+
+#include <kcombobox.h>
+
+// Local includes
+
+#include "libkexiv2_export.h"
+
+namespace KExiv2Iface
+{
+
+class CountrySelectorPriv;
+
+class KEXIV2_EXPORT CountrySelector : public KComboBox
+{
+
+public:
+
+    CountrySelector(QWidget* parent);
+    ~CountrySelector();
+
+    void setCountry(const QString& countryCode);
+    bool country(QString& countryCode, QString& countryName);
+
+    static QString countryForCode(const QString& countryCode);
+
+private:
+
+    CountrySelectorPriv* const d;
+};
+
+}  // namespace KExiv2Iface
+
+#endif // COUNTRY_SELECTOR_H
