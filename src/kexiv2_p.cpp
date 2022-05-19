d0d6b191ebc1bdd2d1b722ee243b09abac5e5629
Nicolas Fella <nicolas.fella@gmx.de>
2022-05-19 22:38:29 +0200

    Remove usage of register keyword
    It serves no purpose and is invalid C++17
    
    BUG: 454050

--- src/kexiv2_p.cpp
+++ src/kexiv2_p.cpp
@@ -437,7 +437,7 @@ QString KExiv2Private::detectEncodingAndDecode(const std::string& value) const
 bool KExiv2Private::isUtf8(const char* const buffer) const
 {
     int i, n;
-    register unsigned char c;
+    unsigned char c;
     bool gotone = false;
 
     if (!buffer)
